8d33b997fe95de907cd9394e73d0d27c0d8e1f88
gui-lherme1234 <donizetti.g@aluno.ifsp.edu.br>
2024-05-13 08:38:30 -0300

    Atualização do dia 13/05/2024

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,16 @@
+/*
+index: listagem de sessões
+store(add): criar uma sessão
+show: quando queremos alterar um ÚNICA sessão
+update: quando queremos aletrar alguma sessão
+destroy(delete): quando queremos deletar uma sessão
+*/
+
+import User from '../models/User'
+import * as Yup from 'yup'
+
+class SessionController {
+    async store(req,res) {
+        
+    }
+}
